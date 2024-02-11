bc828399283a593cf86178edcd6893101d44991b
Devadathan M B <devadathanmb@gmail.com>
2024-02-11 10:47:30 +0530

    fix: Add electronAPI back to exposed API's to renderer

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,4 +1,5 @@
 import { contextBridge, ipcRenderer } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
 const api = {
@@ -13,10 +14,14 @@ const api = {
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('api', api)
+
+    contextBridge.exposeInMainWorld('electron', electronAPI)
   } catch (error) {
     console.error(error)
   }
 } else {
+  // @ts-ignore (define in dts)
+  window.electron = electronAPI
   // @ts-ignore (define in dts)
   window.api = api
 }
